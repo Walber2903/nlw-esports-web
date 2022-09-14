b1ea1f19804b4fa2f6df19fecae1d03f70d3f4a2
walber2903 <walber.menezes@hotmail.com>
2022-09-14 16:55:17 -0300

    Started HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import './styles/main.css';
+import logoImg from './assets/logo-nlw.png'
+
+
 function App() {
-  return <h1>Hellor NLW!</h1>
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="Logo do NLW eSports" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'> duo </span> está aqui.</h1>
+    </div>
+  )
 }
 
 export default App
